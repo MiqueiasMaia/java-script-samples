c8daba614910b400766895aa3510756b35493a51
Miqueias Maia <miqueiasmaia7@gmail.com>
2021-10-25 17:15:08 -0300

    first commit

---
+++ content.js
@@ -0,0 +1,24 @@
+/**
+ * Tipos numéricos
+ * Tipo boolean
+ * Tipo string (aspas simples, duplas, craseado, concatenação)
+ * Conversão de tipos
+ */
+
+/**
+ * VETORES
+ */
+
+/**
+ * Estruturas de DECISÃO e TESTES LÓGICOS
+ * IF
+ * SWITCH CASE
+ */
+
+/**
+ * Estruturas de REPETIÇÃO
+ * WHILE
+ * DO WHILE
+ * FOR
+ * FOREACH
+ */
