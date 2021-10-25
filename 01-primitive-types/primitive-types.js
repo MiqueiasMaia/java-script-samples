e3d4db65aa2cfd67f4943b9ec2640e3783c947d7
Miqueias Maia <miqueiasmaia7@gmail.com>
2021-10-25 20:48:14 -0300

    feat: add primitive types

---
+++ 01-primitive-types/primitive-types.js
@@ -0,0 +1,10 @@
+var numberA = 1;
+var numberB = 7;
+
+var operationA = numberA + numberB;
+var operationB = numberA / numberB;
+var operationC = numberA * numberB;
+var operationD = numberA - numberB;
+var operationE = numberA % numberB;
+
+console.log(operationA, operationB, operationC, operationD, operationE);
