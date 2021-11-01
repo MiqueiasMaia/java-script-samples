9fd7cd63e3d95665ae25154af521b910c4734670
Miqueias Maia <miqueiasmaia7@gmail.com>
2021-11-01 19:14:53 -0300

    feat: update with castings and variabe types

--- 01-Basics/04-parsers/01-parsers.js
+++ 01-Basics/04-parsers/01-parsers.js
@@ -1,5 +1,15 @@
-const number = 456;
-const numberString = "456";
+// const number = 456;
+// const numberString = "456";
 
-console.log(Number(numberString) + number);
-console.log(+ numberString + number);
+// console.log(Number(numberString) + number);
+// console.log(+ numberString + number);
+
+let numb = {
+    value: 200
+};
+const numbS = {
+    value: 150
+};
+numbS.value = 200;
+console.log(numb.value == numbS.value); //true
+//console.log(+numb === numbS);
