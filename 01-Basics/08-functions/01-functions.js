e75c7ecd8b59051a9ccd9e42654bf2f0483ea943
Miqueias Maia <miqueiasmaia7@gmail.com>
2021-11-01 20:34:31 -0300

    feat: add examples

--- 01-Basics/08-functions/01-functions.js
+++ 01-Basics/08-functions/01-functions.js
@@ -1,21 +1,43 @@
+// var var1 = "1";
+// var var2 = "9";
+
+// var concat = concact(var1, var2);
+// var calcul = calc(var1, var2);
+
+// console.log(concat, calcul);
+
+// function concact(var1, var2) {
+//   return var1 + var2;
+// }
+
+// function calc(var1, var2 = 100) {
+//   return Number(var1) + Number(var2);
+// }
+
+// printer(calc(98, -567) + calc(98));
+
+// function printer(value) {
+//   return console.log(`The value is ${value}`);
+// }
+
 var var1 = "1";
 var var2 = "9";
 
-var concat = concact(var1, var2);
-var calcul = calc(var1, var2);
-
-console.log(concat, calcul);
+printer(concact(var1, var2));
+printer(calc(var1));
 
 function concact(var1, var2) {
-  return var1 + var2;
+    return var1 + var2;
 }
 
-function calc(var1, var2 = 100) {
-  return Number(var1) + Number(var2);
+function calc(var1, var2) {
+    return (+var1) + (+var2);
 }
 
-printer(calc(98, -567) + calc(98));
+function parser(var1) {
+    return Number(var1);
+}
 
 function printer(value) {
-  return console.log(`The value is ${value}`);
+    return console.log(`The value is ${value}`);
 }
